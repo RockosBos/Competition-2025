7a9554129d8caba515f77b188e8c399f2b2c92dd
RockosBos <32206608+RockosBos@users.noreply.github.com>
2025-03-13 18:37:39 -0400

    SERVOOOOOOOO

--- src/main/java/frc/robot/Commands/ClimbClimbingPosition.java
+++ src/main/java/frc/robot/Commands/ClimbClimbingPosition.java
@@ -30,7 +30,11 @@ public class ClimbClimbingPosition extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    if (!interrupted) {
+      c_Climb.setServoClimbingToLAPos(Constants.SERVO_LOCKED);
+    }
+  }
 
   // Returns true when the command should end.
   @Override
